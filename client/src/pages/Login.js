62710051df97c5ce9a6e0302667373a571fac6d9
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2022-12-30 21:32:38 +0530

    Update login and start script

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,8 +10,9 @@ const Login = () => {
   const { user, logIn } = useAuth();
   console.log("user", user);
   const navigate = useNavigate();
-  const onSignIn = async (user) => {
-    const id_token = await user.getAuthResponse().id_token;
+  const onSignIn = async (response) => {
+    console.log(response);
+    const id_token = await response.getAuthResponse().id_token;
     Cookies.set("authtoken", id_token, { expires: 7 });
     const result = await loginApi(id_token);
     console.log(result);
@@ -32,7 +33,7 @@ const Login = () => {
         clientId="703956863946-em6dt95q7vcjrock2qbkvafa6ge057kq.apps.googleusercontent.com"
         render={(props) => <GoogleButton type="dark" onClick={props.onClick} />}
         onSuccess={onSignIn}
-        onFailure={() => console.log("Failed")}
+        onFailure={onSignIn}
         cookiePolicy={"single_host_origin"}
         buttonText="Login"
       />
